027110797bd90d438fbed030a7b6f53691fd6b04
MarkusPuura <125037889+MarkusPuura@users.noreply.github.com>
2024-01-15 14:17:39 +0100

    Add files via upload

--- mots_plus_utilises.py
+++ mots_plus_utilises.py
@@ -7,6 +7,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Chargement des données
 df = pd.read_csv('ExtractedTweets.csv')
@@ -30,20 +31,27 @@ df['ProcessedTweet'] = df['Tweet'].apply(preprocess_text)
 X_train, X_test, y_train, y_test = train_test_split(df['ProcessedTweet'], df['Party'], test_size=0.2, random_state=42)
 
 # Classification avec un classificateur SVM linéaire
-vectorizer = CountVectorizer()
+vectorizer = TfidfVectorizer(min_df=5)  # Vous pouvez ajuster min_df selon vos besoins
 X_train_vectorized = vectorizer.fit_transform(X_train)
-classifier = LinearSVC(dual=False, max_iter=10000)
+
+classifier = LinearSVC(dual=False, max_iter=10000, C=1.0)  # Vous pouvez ajuster C selon vos besoins
 classifier.fit(X_train_vectorized, y_train)
 
 # Utiliser le CountVectorizer pour obtenir les mots originaux
 feature_names = np.array(vectorizer.get_feature_names_out())
 
+
 # Obtenir les indices des 5 premiers mots les plus fréquemment utilisés par les démocrates
 democrat_indices = np.argsort(classifier.coef_[0])[-5:][::-1]
 democrat_words = feature_names[democrat_indices]
-print(f"Top 5 words used by Democrats: {democrat_words}")
+print(f"Top 5 words used by Republicans: {democrat_words}")
 
 # Obtenir les indices des 5 premiers mots les plus fréquemment utilisés par les républicains
 republican_indices = np.argsort(classifier.coef_[0])[:5]
 republican_words = feature_names[republican_indices]
-print(f"Top 5 words used by Republicans: {republican_words}")
+print(f"Top 5 words used by Democrats: {republican_words}")
+
+# Afficher les coefficients associés à ces mots
+print("Coefficients for Republican words:", classifier.coef_[0, democrat_indices])
+print("Coefficients for Democrats words:", classifier.coef_[0, republican_indices])
+
